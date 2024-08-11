e5a9c6d069ade77e7523acebd6baa94fae0ed859
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-11 11:18:59 +0300

    Completed transforming the list of items into a list of components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link, index) => {
+    return <a key={index} href={`#${link}`}>{link}</a>;
+  });
+  return <nav>{/* display an <a> tag for each link here */}
+       <ul>
+       {linkElements}
+       </ul>
+        
+  
+  </nav>;
 }
 
 export default NavBar;
